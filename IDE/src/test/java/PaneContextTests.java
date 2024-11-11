a94c3e252c569ba4fe687466766a5da9e2d7f6d2
RuthsGarrett <ruthsgarrett@gmail.com>
2024-11-10 19:40:58 -0500

    Add GBT tests for deleteUnusedScreenshots

--- IDE/src/test/java/PaneContextTests.java
+++ IDE/src/test/java/PaneContextTests.java
@@ -1,3 +1,4 @@
+import org.junit.BeforeClass;
 import org.sikuli.ide.EditorPane;
 import org.sikuli.ide.SikulixIDE;
 import org.junit.Before;
@@ -5,209 +6,159 @@ import org.junit.Test;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 
 public class PaneContextTests extends SikulixIDE {
 
-    private SikulixIDE ide;
-    private SikulixIDE.PaneContext pane;
+    private static SikulixIDE ide;
+    private static SikulixIDE.PaneContext pane;
 
-    @Before
-    public void setUp() {
+    @BeforeClass
+    public static void setUp() {
         SikulixIDE.start();
         ide = SikulixIDE.get();
         pane = ide.getContext();
     }
 
-    @Test
-    public void testPaneContextgetPane() {
-        EditorPane example = pane.getPane();
-        //todo
+    File directorySetup(String directory, Set<String> images)
+    {
+        File Dir = new File(directory);
+        Dir.mkdir();
+        for(String image : images)
+        {
+            File f = new File(directory+"\\"+image);
+            try
+            {
+                f.createNewFile();
+            }
+            catch ( IOException e)
+            {
+                fail("Couldn't create files");
+            }
+        }
+        return Dir;
     }
 
     @Test
-    public void testfocus() {
-
+    public void deleteNotUsedScreenshots_noDirectory() {
+        File Dir = new File("I wont exist");
+        try {
+            pane.deleteNotUsedScreenshots(Dir,null);
+        }
+        catch (Exception e)
+        {
+            fail("Exception caught when passing an invalid dir to function");
+        }
     }
 
     @Test
-    public void testgetPane() {
-
+    public void deleteNotUsedScreenshots_noFiles() {
+        Set<String> images = new HashSet<String>();
+        File Dir = new File("./tmp");
+        Dir.mkdir(); //I'm assuming you will work
+        try {
+            pane.deleteNotUsedScreenshots(Dir,images);
+            if(!Dir.exists())
+            {
+                fail("Invalid test since the folder didnt exist");
+            }
+        }
+        catch (Exception e)
+        {
+            fail("Exception caught when passing an invalid dir to function");
+        }
+        Dir.delete();
     }
 
     @Test
-    public void testisValid() {
-
+    public void deleteNotUsedScreenshots_usedFiles() {
+        Set<String> images = new HashSet<String>();
+        String dirName = "tmp";
+        images.add("tmp1.jpg");
+        images.add("tmp2.jpg");
+        images.add("tmp3.jpg");
+        File Dir = directorySetup(dirName, images);
+        try {
+            pane.deleteNotUsedScreenshots(Dir,images);
+            if(!Dir.exists())
+            {
+                fail("Invalid test since the folder didnt exist");
+            }
+            assertTrue("Wrong file count ",Dir.listFiles().length == 3); //Function shouldn't delete anything, since they were used
+        }
+        catch (Exception e)
+        {
+            fail("Exception caught when passing an invalid dir to function");
+        }
+        for(File f : Dir.listFiles())
+        {
+            f.delete();
+        }
+        Dir.delete();
     }
 
     @Test
-    public void testisText() {
-
+    public void deleteNotUsedScreenshots_unusedFiles() {
+        Set<String> images = new HashSet<String>();
+        Set<String> none = new HashSet<String>();
+        String dirName = "./tmp";
+        images.add("tmp1.jpg");
+        images.add("tmp2.jpg");
+        images.add("tmp3.jpg");
+        File Dir = directorySetup("./tmp", images);
+        try {
+            pane.deleteNotUsedScreenshots(Dir,none);
+            if(!Dir.exists())
+            {
+                fail("Invalid test since the folder didnt exist");
+            }
+            assertTrue("Wrong file count " + Dir.listFiles().length, Dir.listFiles().length == 0); //Function should have deleted every file, since none were used
+        }
+        catch (Exception e)
+        {
+            fail("Exception caught when passing an invalid dir to function");
+        }
+        for(File f : Dir.listFiles())
+        {
+            f.delete();
+        }
+        Dir.delete();
     }
 
     @Test
-    public void testisTemp() {
-
-    }
-
-    @Test
-    public void testhasContent() {
-
-    }
-
-    @Test
-    public void testgetSupport() {
-
-    }
-
-    @Test
-    public void testgetType() {
-
-    }
-
-    @Test
-    public void testgetFolder() {
-
-    }
-
-    @Test
-    public void testgetFile() {
-
-    }
-
-    @Test
-    public void testgetFileName() {
-
-    }
-
-    @Test
-    public void testgetExt() {
-
-    }
-
-    @Test
-    public void testisBundle() {
-
-    }
-
-    @Test
-    public void testisBundleFile() {
-
-    }
-
-    @Test
-    public void testgetImageFolder() {
-
-    }
-
-    @Test
-    public void testsetImageFolder() {
-        //Good for GBT
-    }
-
-    @Test
-    public void testgetScreenshotFolder() {
-
-    }
-
-    @Test
-    public void testgetScreenshotFileString() {
-        //Decent for GBT
-    }
-
-    @Test
-    public void testgetScreenshotFileFile() {
-
-    } //the one with the args
-
-    @Test
-    public void testgetShowThumbs() {
-
-    }
-
-    @Test
-    public void testsetShowThumbs() {
-
-    }
-
-    @Test
-    public void testclose() {
-
-    }
-
-    @Test
-    public void testcloseSilent() {
-
-    }
-
-    @Test
-    public void testdoClose() {
-
-    }
-
-    @Test
-    public void testdeleteNotUsedImages() {
-
-    }
-
-    @Test
-    public void testdeleteNotUsedScreenshots() {
-        //Decent for GBT
-    }
-
-    @Test
-    public void testisDirty() {
-
-    }
-
-    @Test
-    public void testsetDirty() {
-
-    }
-
-    @Test
-    public void testnotDirty() {
-
-    }
-
-    @Test
-    public void testsetFileTabTitleDirty() {
-
-    }
-
-    @Test
-    public void testsave() {
-
-    }
-
-    @Test
-    public void testsaveAs() {
-
-    }
-
-    @Test
-    public void testload() {
-        //Good for Domain Modeling
-    }
-
-    @Test
-    public void testloadFile() {
-
-    }
-
-    @Test
-    public void testloadContext() {
-
-    }
-
-    @Test
-    public void testinsertImageButton() {
-
-    }
-
-    @Test
-    public void testresetContenttotext() {
-
+    public void deleteNotUsedScreenshots_mixedFiles() {
+        Set<String> images = new HashSet<String>();
+        String dirName = "tmp";
+        images.add("tmp1.jpg");
+        images.add("tmp2.jpg");
+        images.add("tmp3.jpg");
+        images.add("tmp4.jpg");
+        images.add("tmp5.jpg");
+        File Dir = directorySetup(dirName, images);
+        Set<String> newimages = new HashSet<String>();
+        newimages.add("tmp1.jpg");
+        newimages.add("tmp2.jpg");
+        try {
+            pane.deleteNotUsedScreenshots(Dir,newimages);
+            if(!Dir.exists())
+            {
+                fail("Invalid test since the folder didnt exist");
+            }
+            assertTrue("Wrong file count " + Dir.listFiles().length,Dir.listFiles().length == 2); //Function should leave 2 photos
+        }
+        catch (Exception e)
+        {
+            fail("Exception caught when passing an invalid dir to function");
+        }
+        for(File f : Dir.listFiles())
+        {
+            f.delete();
+        }
+        Dir.delete();
     }
 }
